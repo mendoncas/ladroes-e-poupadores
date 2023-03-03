91d6092c260b11ee9b1bbb9b9e0f5227376e94f6
730549390 <ricardocesarfilho@unifor.br>
2023-03-03 17:29:44 -0300

    pqp caralho

--- src/algoritmo/Poupador.java
+++ src/algoritmo/Poupador.java
@@ -2,62 +2,91 @@ package algoritmo;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.Map;
+import java.util.Queue;
 
 public class Poupador extends ProgramaPoupador {
 	int [][] mapaExplorado = new int[31][31];
+	// 0: parado; 1: acima; 2: abaixo; 3: direita; 4: esquerda;
+	int [] direcaoParaVisao = {0, 7, 16, 12, 11};
+	int [] direcaoParaOlfato = {0, 1, 6, 4, 3};
+	// Fila contendo os últimos nós visitados
+	Queue<int []> ultimosVisitados = new LinkedList<>();
+	int [] ultimoVisitado;
 	
-	// TODO explorar - pesos maiores pra quadrados explorados
+	// DONE explorar - prioridades maiores pra quadrados inexplorados
+	// TODO refatorar função lerQuadradoMapaExplorado para utilizar um vetor
+	// TODO Função de chance para explorar considera mais pesos (cheiro de ladroes, presença de moedas, etc)
+	// TODO Buscar o banco: poupadores buscam caminho para o banco
+	// TODO Isca: um poupador com 0 moedas, ao final do jogo, distrai ladroes
+	// TODO Medo: um poupador se sente mais seguro quanto mais próximo do banco
 	
 
 	public int acao() {
-		// acima = 7, abaixo = 16, esquerda = 11, direita = 12
 		// acima = 1, abaixo = 2, esquerda = 4, direita = 3
 		int[] direcoes = {0, 1, 2, 3, 4};
-		// acima = 7, abaixo = 16, esquerda = 11, direita = 12
-		int[] visao = {0, 7, 16, 12, 11};
 		ArrayList<Integer> direcoesComPeso = new ArrayList<Integer>();
 		
 		// marcar posição como explorada
 		this.mapaExplorado
 				[sensor.getPosicao().x]
-				[sensor.getPosicao().y] += 3;
+				[sensor.getPosicao().y] += 2;
 		
 		for(int i=0; i< direcoes.length; i++) {
 			int direcao = direcoes[i];
-			int chance = (int) 10/(this.lerQuadradoMapaExplorado(direcao)+1);
+			int chance = 1 + avaliarQuadrado(direcao);
 			
 			for(int j=0; j<chance; j++) {
 				direcoesComPeso.add(direcao);
 			}
 		}
 		
-		return this.escolherPasso(direcoesComPeso);
+		return this.escolherDirecaoAleatoria(direcoesComPeso);
 	}
 	
-	public int escolherPasso(ArrayList<Integer> direcoesComPeso) {
+	public int avaliarQuadrado(int direcao) {
+		
+		System.out.println("olfato: " + sensor.getAmbienteOlfatoLadrao() [this.direcaoParaOlfato[direcao]]);
+		int peso = (this.lerQuadradoMapaExplorado(direcao))
+				+ 50/(sensor.getAmbienteOlfatoLadrao() [this.direcaoParaOlfato[direcao]] + 1);
+		return (int) 100/(peso + 1);
+	}
+
+	public int escolherDirecaoAleatoria(ArrayList<Integer> direcoesComPeso) {
+		System.out.println(direcoesComPeso.toString());
+
 		int direcao = direcoesComPeso.get((int) (Math.random() * direcoesComPeso.size()));
+
+		while(
+				sensor.getVisaoIdentificacao()[this.direcaoParaVisao[direcao]] < 0 ||
+				sensor.getVisaoIdentificacao()[this.direcaoParaVisao[direcao]] == 1) {
+
+			direcao = direcoesComPeso.get((int) (Math.random() * direcoesComPeso.size()));
+		}
+
+		this.ultimoVisitado = this.getCoordenadasDoQuadrado(direcao);
 		return direcao;
 	}
 	
-	// recebe um quadrado de acordo com o mapa da visão e pega seu valor no mapaExplorado
+	// recebe um quadrado no alcance do agente (0, 1, 2, 3, 4) e pega seu valor no mapaExplorado
 	public int lerQuadradoMapaExplorado(int quadradoMapaVisao) {
-		// acima = 7, abaixo = 16, esquerda = 11, direita = 12
-		Map<Integer, int[]> transform = new HashMap<>();
-		transform.put(0, new int[]{0, 0});
-		transform.put(1, new int[]{0, -1});
-		transform.put(2, new int[]{-1, 0});
-		transform.put(3, new int[]{1, 0});
-		transform.put(4, new int[]{0, 1});
-		int[] coordenadas = transform.get(quadradoMapaVisao);
-
-		int[] coordenadasMapa = {sensor.getPosicao().x + coordenadas[0], sensor.getPosicao().y + coordenadas[1]};
+		int[] coordenadasMapa = this.getCoordenadasDoQuadrado(quadradoMapaVisao);
 		try {
 			return mapaExplorado[coordenadasMapa[0]][coordenadasMapa[1]];
 		}
 		catch(Exception e) {
-			return mapaExplorado[coordenadasMapa[0]+1][coordenadasMapa[1]+1];
+			return 300;
+//			return mapaExplorado[coordenadasMapa[0]+1][coordenadasMapa[1]+1];
 		}
 	}
 	
+	// recebe um quadrado no alcance do agente (0, 1, 2, 3, 4) e retorna suas coordenadas
+	public int[] getCoordenadasDoQuadrado(int direcao) {
+		int[][] transform = {{0,0}, {0,-1}, {-1,0}, {1,0}, {0,1}};
+		int[] coordenadas = transform[direcao];
+		
+		int[] coordenadasMapa = {sensor.getPosicao().x + coordenadas[0], sensor.getPosicao().y + coordenadas[1]};
+		return coordenadasMapa;
+	}
 }
